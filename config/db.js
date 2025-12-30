2471a03b88a594a2562d908535100ff0e51dd887
Javier Sanz <javiersanzvilloldo@gmail.com>
2025-12-30 13:43:45 +0100

    Update database connection to use environment variables

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : 'root',
-  database : 'red_social'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'red_social'
 });
 
 connection.connect((err) =>{
