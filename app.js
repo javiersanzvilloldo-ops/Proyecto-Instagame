c43c1c001bb7dc07b3128b43d23bf7f7cd20048b
Javier Sanz <javiersanzvilloldo@gmail.com>
2025-12-30 13:48:48 +0100

    Add dotenv configuration to app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+require("dotenv").config();
+  
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const gameRouter = require('./routes/game');
